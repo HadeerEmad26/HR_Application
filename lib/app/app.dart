27589c9408278f37a8e3134cd03be39e800dbba4
pc2023 <hadeere378@gmail.com>
2025-01-20 21:52:49 +0200

    en & ar files

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../core/routes/app_routes.dart';
+
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +14,16 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       builder:  (context, child) => const MaterialApp(
         debugShowCheckedModeBanner: false,
-         initialRoute: Routes.intialRoutes,
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+         supportedLocales: [
+           Locale("en"),
+           Locale("ar")
+         ],
+        initialRoute: Routes.intialRoutes,
         onGenerateRoute: AppRoutes.generateRoute,
       ),
     );
